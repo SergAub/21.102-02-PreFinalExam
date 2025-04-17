bf335bf65c2f467da7e9623e3ac3fd86db12ad9b
SergAub <92084092+SergAub@users.noreply.github.com>
2025-04-17 10:36:49 +0700

    Вроде все

--- 21.102-02-PreFinalExam/View/AddClient.xaml.cs
+++ 21.102-02-PreFinalExam/View/AddClient.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using _21._102_02_PreFinalExam.dbModel;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,21 +22,67 @@ namespace _21._102_02_PreFinalExam.View
     /// </summary>
     public partial class AddClient : Window
     {
+        string _photoPath;
         public AddClient()
         {
             InitializeComponent();
             btnCancel.Click += BtnCancel_Click;
             btnOK.Click += BtnOK_Click;
+            btnChoosePhoto.Click += BtnChoosePhoto_Click;
+        }
+
+        private void BtnChoosePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == true)
+            {
+                _photoPath = openFileDialog.FileName;
+                tblPhotoPath.Text = _photoPath;
+            }
         }
 
         private void BtnOK_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            MessageBoxResult messageBoxResult = MessageBox.Show("Сохранить изменения?",
+                "Сохранение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (messageBoxResult != MessageBoxResult.Yes) return;
+
+            try
+            {
+                if (tbSurname.Text.Length == 0) throw new Exception("Пустая фамилия");
+                if (tbName.Text.Length == 0) throw new Exception("Пустое имя");
+                if (tbPhone.Text.Length == 0) throw new Exception("Пустое имя");
+
+                if (!Regex.IsMatch(tbPhone.Text, @"^\d{10,15}$")) throw new Exception("Неверный формат телефона");
+
+                using (Entities db = new Entities())
+                {
+                    Clients client = new Clients() { 
+                        Surname = tbSurname.Text,
+                        Name = tbName.Text,
+                        Patronymic = tbPhone.Text,
+                        Phone = tbPhone.Text,
+                        Photo = _photoPath,
+                    };
+                    db.Clients.Add(client);
+                    db.SaveChanges();
+                }
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void BtnCancel_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            MessageBoxResult messageBoxResult = MessageBox.Show("Отменить изменения?",
+                "Отмена", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
         }
     }
 }
